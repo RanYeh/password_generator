eaa1b964a73020b7feddf6eb618790126c8e821f
RanYeh <kidwiss@gmail.com>
2023-03-27 23:47:09 +0800

    feat: finish function generate_password

--- app.js
+++ app.js
@@ -4,23 +4,22 @@ const exphbs = require('express-handlebars')
 
 const app = express()
 
-
 //-- Set template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 //-- Set bodyParser
-
-
-//-- Set static files
-
-
+app.use(express.urlencoded({ extended: true }))
 
 //-- Set routes
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.render('index')
+})
 
 //-- Start and listen on the express server
 app.listen(3000, () => {
---
+++ generate_password.js
@@ -0,0 +1,59 @@
+// define generatePassword function
+function generatePassword() {
+  // define things user might want
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
+
+  // define dummy data
+  const options = {
+    length: 12,
+    lowercase: 'on',
+    uppercase: 'on',
+    numbers: 'on',
+    // symbols: 'on',
+    excludeCharacters: '40A'
+  }
+
+  // create a collection to store things user picked up
+  let collection = []
+
+  if (options.lowercase === 'on') {
+    collection = collection.concat([...lowerCaseLetters])
+  }
+
+  if (options.uppercase === 'on') {
+    collection = collection.concat([...upperCaseLetters])
+  }
+
+  if (options.numbers === 'on') {
+    collection = collection.concat([...numbers])
+  }
+
+  if (options.symbols === 'on') {
+    collection = collection.concat([...symbols])
+  }
+
+  // remove things user do not need
+  if (options.excludeCharacters) {
+    collection = collection.filter(char => !options.excludeCharacters.includes(char))
+  }
+
+  // start generating password
+  let password = ''
+  for (let i = 0; i < options.length; i++) {
+    password += sample(collection) 
+  }
+
+  // return the generated password
+  return password
+}
+
+// define sample function to randomly return an item in an array
+function sample(array) {
+  let randomIndex = Math.floor(Math.random() * array.length)
+  return array[randomIndex]
+}
+
+generatePassword()
